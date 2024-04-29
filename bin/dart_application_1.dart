2e8d45a8f396d9e9a0131188f724032444d5ceca
Al-Amin134 <s2110676134@ru.ac.bd>
2024-04-29 10:43:07 +0600

    variable diclarations

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Al-Amin";
+  int my_age = 22;
+  bool isGraduate = false;
+  double percentage = 85.5;
+  dynamic name1 = 'babu'; // dynamic er jnno ja kichu type neya jay.
+
+  print(name);
+  print(my_age);
+  print(isGraduate);
+  print(percentage);
+  print(name1);
+}
